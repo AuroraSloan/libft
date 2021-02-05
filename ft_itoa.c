704ebb43d76fe7f3a75d4880850515cf59e6e2f0
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-06 00:59:06 +0900

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,27 +1,27 @@
 #include "libft.h"
 
-char        *ft_itoa(int n)
+char    *ft_itoa(int n)
 {
-    char			*str;
-    int				len;
-    unsigned int	tmp;
+    char            *str;
+    int             len;
+    unsigned int    tmp;
 
     len = 0;
     tmp = (n < 0) ? -n : n;
     while (tmp)
-    {   
-        tmp /= 10; 
+    {
+        tmp /= 10;
         len++;
-    }   
+    }
     len = n <= 0 ? len + 1 : len;
     if (!(str = (char*)malloc(sizeof(*str) * (len + 1))))
         return (NULL);
     str[len--] = '\0';
     tmp = (n < 0) ? -n : n;
     while (tmp)
-    {   
-		str[len--] = tmp % 10 + '0';
-		tmp /= 10;
+    {
+        str[len--] = tmp % 10 + '0';
+        tmp /= 10;
 	}
 	if (n <= 0)
 		str[0] = (n == 0) ? '0' : '-';
