4874693f3974209c88c4b0ebd89cc107ff8b5478
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-05 15:41:33 +0900

    Create ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,23 @@
+int         ft_atoi(const char *str)
+{
+    int				i;
+    long long int	nbr;
+    long long int	sign;
+
+    i = 0;
+    nbr = 0;
+    sign = 1;
+    while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == '\f' || str[i] == '\r' || str[i] == '\v')
+        i++;
+    if (str[i] == '-' || str[i] == '+')
+        if (str[i++] == '-')
+    		sign = -1; 
+    while (str[i] >= '0' && str[i] <= '9' && str[i])
+    {   
+        nbr = nbr * 10 + (str[i++] - '0');
+        if (nbr < 0)
+			return ((sign + 1) / -2);
+    }   
+    return ((int)(nbr * sign));
+}
