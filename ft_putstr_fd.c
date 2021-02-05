537e5500f521f86c7dd526c5963c234d7c41e4bf
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-06 01:56:36 +0900

    Update ft_putstr_fd.c

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -2,11 +2,8 @@
 
 void    ft_putstr_fd(char *s, int fd) 
 {
-    int i;
-
-    i = 0;
-    if (s == NULL)
+    if (!s)
         return ;
-    while (s[i])
-        ft_putchar_fd(s[i++], fd);
+    while (*s)
+        ft_putchar_fd(*s++, fd);
 }
