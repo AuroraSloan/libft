280f6254bd5cd28fff93289ffe8d5b1efec360e8
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-06 01:23:37 +0900

    Create ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,16 @@
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    const unsigned char *s1_ptr;
+    const unsigned char *s2_ptr;
+
+    s1_ptr = s1; 
+    s2_ptr = s2; 
+    while (n-- > 0)
+    {   
+        if (*s1_ptr != *s2_ptr)
+            return (*s1_ptr - *s2_ptr);
+        s1_ptr++;
+        s2_ptr++;
+    }   
+    return (0);
+}
