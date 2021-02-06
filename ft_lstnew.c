4f580b9219620b028d28ec0b2ba1f47709f03b40
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-06 19:02:53 +0900

    Update ft_lstnew.c

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -4,7 +4,7 @@ t_list  *ft_lstnew(void *content)
 {
     t_list  *new_list;
 
-    if (!(new_list = malloc(sizeof(t_list)))
+    if (!(new_list = malloc(sizeof(t_list))))
         return (NULL);
     new_list->content = content;
     new_list->next = NULL;
