364446b32defc8d5ed5b3e19483743e4c2460d0f
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-07 02:16:43 +0900

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -11,7 +11,7 @@ int	ft_atoi(const char *str)
 	sign = 1;
 	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
 		str[i] == '\f' || str[i] == '\r' || str[i] == '\v')
-			i++;
+		i++;
 	if (str[i] == '-' || str[i] == '+')
 		if (str[i++] == '-')
 			sign = -1;
