66deaabc48a5ba2f1e5a5389eb8cb0cb0f440db1
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-07 01:40:01 +0900

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,9 +1,9 @@
 #include "libft.h"
 
-char    *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-    char *ret;
+	char	*ret;
 
-    ret = ft_memchr(s, c, (ft_strlen(s) + 1));
-    return (ret);
+	ret = ft_memchr(s, c, (ft_strlen(s) + 1));
+	return (ret);
 }
