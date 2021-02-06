3a6158dcc5b9e4a27fb10619074e36fad747f052
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-07 02:08:56 +0900

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -5,8 +5,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	const unsigned char	*s1_ptr;
 	const unsigned char	*s2_ptr;
 
-	s1_ptr = (const unsigned char *)s1; 
-	s2_ptr = (const unsigned char *)s2; 
+	s1_ptr = (const unsigned char *)s1;
+	s2_ptr = (const unsigned char *)s2;
 	while (n-- > 0)
 	{
 		if (*s1_ptr != *s2_ptr)
