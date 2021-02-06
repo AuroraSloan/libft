e6c61912150b443cbdb7df94a60d0f22c99a06df
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-06 23:48:35 +0900

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -5,21 +5,21 @@ int ft_atoi(const char *str)
 	int             i;
 	long long int   nbr;
 	long long int   sign;
-
-    i = 0;
-    nbr = 0;
-    sign = 1;
-    while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-		   str[i] == '\f' || str[i] == '\r' || str[i] == '\v')
+	
+	i = 0;
+	nbr = 0;
+	sign = 1;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+	       str[i] == '\f' || str[i] == '\r' || str[i] == '\v')
 		i++;
-    if (str[i] == '-' || str[i] == '+')
+	if (str[i] == '-' || str[i] == '+')
 		if (str[i++] == '-')
-			sign = -1; 
-    while (ft_isdigit(str[i]) && str[i])
-    {
+			sign = -1;
+	while (ft_isdigit(str[i]) && str[i])
+	{
 		nbr = nbr * 10 + (str[i++] - '0');
 		if (nbr < 0)
 			return ((sign + 1) / -2);
-    }   
-    return ((int)(nbr * sign));
+	}
+	return ((int)(nbr * sign));
 }
