0b99ec1c43e61decf6b3d8746a82239a748d1347
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-07 01:42:58 +0900

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,29 +1,29 @@
 #include "libft.h"
 
-char    *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    int     i;  
-    int     j;  
-    int     len;
-    char    *join;
+	int	i;  
+	int	j;  
+	int	len;
+	char	*join;
 
-    if (!s1 || !s2)
-        return ((char*)s1);
-    len = ft_strlen(s1) + ft_strlen(s2);
-    if (!(join = (char *)malloc(sizeof(*join) * len + 1)))
-        return (NULL);
-    i = 0;
-    while (s1[i])
-    {   
-        join[i] = s1[i];
-        i++;
-    }   
-    j = 0;
-    while (s2[j])
-    {
-        join[i + j] = s2[j];
-        j++;
-    }
-    join[i + j] = '\0';
-    return (join);
+	if (!s1 || !s2)
+		return ((char*)s1);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (!(join = (char *)malloc(sizeof(*join) * len + 1)))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		join[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		join[i + j] = s2[j];
+		j++;
+	}
+	join[i + j] = '\0';
+	return (join);
 }
