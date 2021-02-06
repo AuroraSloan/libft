a95f2dc79ffb9d2c7e5e3ba741080bd8674560bf
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-07 01:48:47 +0900

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,20 +1,20 @@
 #include "libft.h"
 
-char    *ft_strmapi(char const *s, char (*f) (unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f) (unsigned int, char))
 {
-    char    *ptr;
-    int     i;  
+	char	*ptr;
+	int	i;  
 
-    i = 0;
-    if (!s)
-        return ((char*)s);
-    if (!(ptr = (char*)malloc(sizeof(*ptr) * ft_strlen(s) + 1)))
-        return (NULL);
-    while (s[i])
-    {   
-        ptr[i] = f(i, s[i]);
-        i++;
-    }   
-    ptr[i] = '\0';
-    return (ptr);
+	i = 0;
+	if (!s)
+		return ((char*)s);
+	if (!(ptr = (char*)malloc(sizeof(*ptr) * ft_strlen(s) + 1)))
+		return (NULL);
+	while (s[i])
+	{   
+		ptr[i] = f(i, s[i]);
+		i++;
+	}   
+	ptr[i] = '\0';
+	return (ptr);
 }
