5163500af594f5d347fc7c5693fb062a541bfd95
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-07 01:17:02 +0900

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,23 +1,23 @@
 #include "libft.h"
 
-t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void*))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void*))
 {
-    t_list *new_list;
-    t_list *new_head;
+	t_list *new_list;
+	t_list *new_head;
 
-    if (!lst || !*f)
-        return (NULL);
-    new_head = NULL;
-    while (lst)
-    {   
-        new_list = ft_lstnew(f(lst->content));
-        if (!new_list)
-        {
-            ft_lstclear(&new_head, del);
-            return (NULL);
-        }
-        ft_lstadd_back(&new_head, new_list);
-        lst = lst->next;
-    }   
-    return (new_head);
+	if (!lst || !*f)
+		return (NULL);
+	new_head = NULL;
+	while (lst)
+	{
+		new_list = ft_lstnew(f(lst->content));
+		if (!new_list)
+		{
+			ft_lstclear(&new_head, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_head, new_list);
+		lst = lst->next;
+	}
+	return (new_head);
 }
