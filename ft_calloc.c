4d90660c54c187d0a6889c8eb5207d64558b3772
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-07 00:49:52 +0900

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,12 +1,12 @@
 #include "libft.h"
 
-void    *ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-    void    *ptr;
+	void	*ptr;
 
-    ptr = malloc(size * count);
-    if (ptr == NULL)
-        return (NULL);
-    ft_bzero(ptr, size * count);
-    return (ptr);
+	ptr = malloc(size * count);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, size * count);
+	return (ptr);
 }
