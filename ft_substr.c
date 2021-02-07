cf2336abe9a068d2e84647340ce79b80af8156b7
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-07 16:33:34 +0900

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -1,3 +1,6 @@
+//Allocates and returns a substring from the string 's'. 
+//The substring begins with the index 'start' and is of maximum size 'len'.
+
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
