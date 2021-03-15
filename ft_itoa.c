c5da86e736e227d400650064aa7cdf1f4ddefc43
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-03-16 00:07:54 +0900

    Add Japanese

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,4 +1,5 @@
-//Allocates and reutrns a string representing the integer received as an arguments.
+//Allocates and returns a string representing the integer that was received as an arguments.
+//整数の引数で代表文字列を確保して返します。
 
 #include "libft.h"
 
