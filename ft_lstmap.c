28b784542b2b35668d2d0eefb9cd79cc34da4540
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-03-17 03:10:01 +0900

    Add Japanese

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,6 +1,9 @@
-//Iterates the list 'lst' and applies the function 'f' to the content of each element.
+//Iterates the list 'lst' and applies the function 'f' to the content of each element
+//'lst'リストの内容一つ一つ'f'関数にします。
 //Creates a new list resulting of the successive applications of the function 'f'.
+//'f'関数にした要素から新しいリストを作ります。
 //The 'del' function is used to delete the content of an element if needed.
+//いれば、'del'関数で要素の内容を消します。
 
 #include "libft.h"
 
