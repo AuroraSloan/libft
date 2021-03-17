1e30d84292a5f9637a79ef76172c4fce1682f702
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-03-17 22:55:02 +0900

    Update ft_lstdelone.c

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -1,6 +1,5 @@
 /*Takes as a parameter an element and frees the memeory of the element's content 
 using the function 'del' given as a parameter and free the element.*/
-//パラメータの'del'関数で入った要素の内容を消してフリーにして、要素をフリーします。
 
 #include "libft.h"
 
