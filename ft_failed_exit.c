379ecbfb3d8e61e011f4c50639cdf2842679f6d1
AuroraSloan <lanajadethompson@gmail.com>
2021-07-23 08:47:52 +0900

    update stderr in failed exit

--- ft_failed_exit.c
+++ ft_failed_exit.c
@@ -7,6 +7,6 @@ void	ft_failed_exit(char *message, char *memory)
 		ft_free(memory);
 		memory = NULL;
 	}
-	ft_putendl_fd(message, 2);
+	ft_putendl_fd(message, STDERR_FILENO);
 	exit (EXIT_FAILURE);
 }
